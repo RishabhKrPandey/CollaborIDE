2bc841c7499a3c35b2973a50754f021bc7e40ed0
RISHABH KUMAR PANDEY <rkp98352@gmail.com>
2025-06-22 15:42:59 +0530

    rendered build from backend

--- backend/index.js
+++ backend/index.js
@@ -200,6 +200,12 @@ app.get("/api/load/:id", async (req, res) => {
     res.status(500).json({ error: "Invalid ID" });
   }
 });
+// serving build from frontend
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
